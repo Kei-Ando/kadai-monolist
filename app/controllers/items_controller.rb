36d4cb5cf9e702ba9b2ad4d68e9d288e2c7d2eb8
Kei Ando <akka1116@yahoo.co.jp>
2017-10-13 11:46:23 +0000

    rakuten api corrected

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
   before_action :require_user_logged_in
-  
+
   def new
     @items = []
-    
+
     @keyword = params[:keyword]
     if @keyword
       results = RakutenWebService::Ichiba::Item.search({
@@ -11,14 +11,14 @@ class ItemsController < ApplicationController
         imageFlag: 1,
         hits: 20,
       })
-      
+
       results.each do |result|
+        # 扱い易いように Item としてインスタンスを作成する（保存はしない）
         item = Item.new(read(result))
-        @item << item
+        @items << item
       end
     end
   end
-
 
   private
 
@@ -27,14 +27,12 @@ class ItemsController < ApplicationController
     name = result['itemName']
     url = result['itemUrl']
     image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
-  
+
     return {
       code: code,
       name: name,
       url: url,
       image_url: image_url,
-   }
+    }
   end
 end
-
-  
